90070161999b07a275af47982922c63905f58748
omarhameed <omarg@dal.ca>
2024-10-25 15:53:59 -0400

    Removed middleware

--- main.py
+++ main.py
@@ -12,13 +12,6 @@ import requests
 
 
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.get("/")
 def read_root():
